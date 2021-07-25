735d9341aa854e85aaf178640f899683070a855f
nguyenduytuan1905 <86639505+nguyenduytuan1905@users.noreply.github.com>
2021-07-25 15:43:58 +0700

    make it better

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from './component/layout/Landing';
+import Auth from './views/Auth';
+import AuthContextProvider from './contexts/AuthContext'
+import Dashboard from './views/Dashboard';
+import ProtectedRoute from './component/routing/ProtectedRoute';
+import About from './views/About';
+import PostContextProvider from './contexts/PostContext';
+function App() {
+  return(
+ <AuthContextProvider>
+   <PostContextProvider>
+   <Router>
+    <Switch>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/login" render={props => <Auth {...props} authRouter='login' />} />
+      <Route exact path="/register" render={props => <Auth {...props} authRouter='register' />} />
+      <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+      <ProtectedRoute exact path="/about" component={About} />
+    </Switch>
+  </Router>
+   </PostContextProvider>
+      
+    </AuthContextProvider>
+  )
+}
+
+export default App;
---
+++ client/src/views/Auth.js
@@ -0,0 +1,44 @@
+import LoginForm from '../component/auth/LoginForm';
+import RegisterForm from '../component/auth/RegisterForm';
+import {AuthContext} from '../contexts/AuthContext'
+import {useContext} from 'react'
+import {Redirect} from 'react-router-dom'
+import Spinner from 'react-bootstrap/Spinner'
+
+const Auth = ({authRouter}) => {
+  const {authState: {authLoading,isAuthenticated}} = useContext(AuthContext)
+
+  let body
+
+  if(authLoading)
+  body = ( 
+    <div className="d-flex-justify-content-center mt-2">
+      <Spinner animation='border' variant='info'/>
+    </div>
+  )
+  else if (isAuthenticated) return <Redirect to='/dashboard' />
+  else
+
+  body = (
+    <>
+
+    {authRouter === 'login' && <LoginForm/>}
+    {authRouter === 'register' && <RegisterForm/>} 
+    </>
+  )
+
+  return (
+   <div className="landing">
+     <div className="dark-overlay">
+        <div className="landing-inner">
+        <h1>LearnIt</h1>
+        <h4>Keep track of what you are learning</h4>
+        {body}
+      </div>
+     </div>
+   </div>
+  )
+
+}
+
+export default Auth
