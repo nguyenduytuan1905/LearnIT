051ce7ecd562bcf89dfa73b6c2d955911387d52f
nguyenduytuan1905 <86639505+nguyenduytuan1905@users.noreply.github.com>
2021-08-05 16:45:56 +0700

    fix api

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,8 +1,8 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'somedeployedURL'
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'https://nguyenduytuan1905-myappreact.netlify.app/api'
 
  export const LOCAL_STORAGE_TOKEN_NAME = 'learnit'
 
- export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS'
+export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS'
 export const POSTS_LOADED_FAIL = 'POSTS_LOADED_FAIL'
 export const ADD_POST = 'ADD_POST'
 export const DELETE_POST = 'DELETE_POST'
